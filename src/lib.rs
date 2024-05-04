01e5578b081a2d9877e848cabe232d249d5ef5de
Adi-df <adidf-web@laposte.net>
2024-05-04 09:10:01 +0200

    Create allocator, index and region structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
 #![no_std]
 
+use core::cell::{RefCell, UnsafeCell};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MemoryRegion {
+    from: usize,
+    size: usize,
+    used: bool,
+}
+
+#[derive(Debug, Clone)]
+pub struct MemoryIndex<const INDEX_SIZE: usize> {
+    regions: [Option<MemoryRegion>; INDEX_SIZE],
+    sorted: bool,
+}
+
+pub struct IndexAllocator<const MEMORY_SIZE: usize, const INDEX_SIZE: usize> {
+    memory: UnsafeCell<[u8; MEMORY_SIZE]>,
+    index: RefCell<MemoryIndex<INDEX_SIZE>>,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
