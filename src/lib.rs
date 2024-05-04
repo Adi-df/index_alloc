40b8164d1067b435d18a0f037eb64ac6b518f705
Adi-df <adidf-web@laposte.net>
2024-05-04 17:16:12 +0200

    Add docs to IndexAllocator

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 //! use index_alloc::IndexAllocator;
 //!
 //! #[global_allocator]
-//! static ALLOCATOR: IndexAllocator<1024,16> = IndexAllocator::empty();
+//! static ALLOCATOR: IndexAllocator<1024, 16> = IndexAllocator::empty();
 //!
 //! fn main() {
 //!     let test_str = String::from("Hello World");
@@ -53,6 +53,20 @@ pub enum IndexError {
     IndexAlreadyBorrowed,
 }
 
+/// The [IndexAllocator] struct is the main component of this crate, it create a memory pool of size `MEMORY_SIZE` with an index of size `INDEX_SIZE`.
+///
+/// There are no restriction on how `MEMORY_SIZE` and `INDEX_SIZE` are set, but `INDEX_SIZE` corresponds to the maximum number of allocated objects that can be held at the same time.
+///
+/// For instance, setting `INDEX_SIZE` to 4 means no more allocations can be performed after 4 boxes are allocated, except if some of them are freed.
+///
+/// [IndexAllocator] implement the [GlobalAlloc] trait wich allows it to be used as the app allocator.
+///
+/// ```rust
+/// use index_alloc::IndexAllocator;
+///
+/// #[global_allocator]
+/// static ALLOCATOR: IndexAllocator<1024, 16> = IndexAllocator::empty();
+///```
 pub struct IndexAllocator<const MEMORY_SIZE: usize, const INDEX_SIZE: usize> {
     memory: UnsafeCell<[u8; MEMORY_SIZE]>,
     index: RefCell<MemoryIndex<INDEX_SIZE>>,
@@ -65,18 +79,26 @@ unsafe impl<const MEMORY_SIZE: usize, const INDEX_SIZE: usize> Sync
 
 impl<const MEMORY_SIZE: usize, const INDEX_SIZE: usize> IndexAllocator<MEMORY_SIZE, INDEX_SIZE> {
     #[must_use]
-    pub const fn new(memory: [u8; MEMORY_SIZE], index: MemoryIndex<INDEX_SIZE>) -> Self {
+    const fn new(memory: [u8; MEMORY_SIZE], index: MemoryIndex<INDEX_SIZE>) -> Self {
         Self {
             memory: UnsafeCell::new(memory),
             index: RefCell::new(index),
         }
     }
 
+    /// Creates an empty [IndexAllocator].
+    /// Inner memory is just zeroes.
+    /// Index is empty.
+    ///
+    /// This should be the standard way to create an [IndexAllocator].
+    ///
+    /// Note that the `MEMORY_SIZE` and `INDEX_SIZE` need to be infered at this point.
     #[must_use]
     pub const fn empty() -> Self {
         Self::new([0; MEMORY_SIZE], MemoryIndex::empty(MEMORY_SIZE))
     }
 
+    /// Try to reserve some [MemoryRegion] based on [Layout] and then return an aligned address (inside the memory pool).
     fn try_reserve(&self, layout: Layout) -> Result<usize, IndexError> {
         let layout = layout.pad_to_align();
         let memory_start = self.memory.get() as usize;
@@ -99,6 +121,7 @@ impl<const MEMORY_SIZE: usize, const INDEX_SIZE: usize> IndexAllocator<MEMORY_SI
         Ok(region.from + allocation_baker.offset)
     }
 
+    /// Try to free some [MemoryRegion] (here the address is the index in the memory pool).
     fn try_free_addr(&self, addr: usize) -> Result<(), IndexError> {
         let mut index = self
             .index
@@ -112,17 +135,20 @@ impl<const MEMORY_SIZE: usize, const INDEX_SIZE: usize> IndexAllocator<MEMORY_SI
         Ok(())
     }
 
+    /// Try to perform allocation based on [Layout], internally uses [IndexAllocator::try_reserve] and then perform pointer arithmetic.
     fn try_alloc(&self, layout: Layout) -> Result<*mut u8, IndexError> {
         let offset = self.try_reserve(layout)?;
         Ok(self.memory.get().cast::<u8>().wrapping_add(offset))
     }
 
+    /// Try to free the [MemoryRegion] associated with the pointer given, internally using [IndexAllocator::try_free_addr].
     fn try_free(&self, ptr: *mut u8) -> Result<(), IndexError> {
         let offset = ptr as usize - self.memory.get() as usize;
         self.try_free_addr(offset)?;
         Ok(())
     }
 
+    /// Try to allocate the value in the memory pool and then return a [Box] smart pointer wich manage the memory.
     pub fn try_boxed<T>(&self, val: T) -> Result<Box<T, MEMORY_SIZE, INDEX_SIZE>, IndexError> {
         Box::try_new(val, self)
     }
