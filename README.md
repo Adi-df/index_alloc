d7dd7be892c17df48da78cd6209d21220481fe32
Adi-df <adidf-web@laposte.net>
2024-05-04 18:15:44 +0200

    Extract main module doc in README.md

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Index Alloc
-A stupid allocator meant to be used in no_std environement as a Global Allocator.
+A simple, toy static Allocator wich use a fixed length array to store allocated data.
+
+This crate expose a struct [`IndexAllocator`] wich implement [`GlobalAlloc`] so it can be uses as the global allocator in `no_std` environement.
+
+Disadvantages :
+- Extremly unsafe
+- Very slow
+- Memory inefficient
+
+Even though it seems unusable, it has plenty of advantages :
+- Just joking don't use that
+
+To store allocated memory, [`IndexAllocator`] uses a `MemoryIndex` wich stores a list of regions containing the state of the region (size, from which address, used status). For instance :
+
+```rust
+use index_alloc::IndexAllocator;
+
+#[global_allocator]
+static ALLOCATOR: IndexAllocator<2048, 16> = IndexAllocator::empty();
+
+fn main() {
+    let test_str = String::from("Hello World");
+    println!("{test_str}");
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,4 @@
-//! A simple, toy static Allocator wich use a fixed length array to store allocated data.
-//!
-//! This crate expose a struct [`IndexAllocator`] wich implement [`GlobalAlloc`] so it can be uses as the global allocator in `no_std` environement.
-//!
-//! Disadvantages :
-//! - Extremly unsafe
-//! - Very slow
-//! - Memory inefficient
-//!
-//! Even though it seems unusable, it has plenty of advantages :
-//! - Just joking don't use that
-//!
-//! To store allocated memory, [`IndexAllocator`] uses a `MemoryIndex` wich stores a list of regions containing the state of the region (size, from which address, used status). For instance :
-//!
-//! ```rust
-//! use index_alloc::IndexAllocator;
-//!
-//! #[global_allocator]
-//! static ALLOCATOR: IndexAllocator<2048, 16> = IndexAllocator::empty();
-//!
-//! fn main() {
-//!     let test_str = String::from("Hello World");
-//!     println!("{test_str}");
-//! }
-//! ```
-
+#![doc = include_str!("../README.md")]
 #![no_std]
 
 use core::alloc::{GlobalAlloc, Layout};
